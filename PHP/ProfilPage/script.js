3c097b216752b556713bcd5823dfa71828dbbaf6
DURUMAIN Xavier <125217354+Xav-duru@users.noreply.github.com>
2024-02-17 15:16:03 +0100

    Delete PHP directory

--- PHP/ProfilPage/script.js
+++
@@ -1,7 +0,0 @@
-function goBackToMovie(user) {
-    // Construire l'URL avec les paramètres
-    var nouvellePageURL = "../SearchPage/searchPage.php?token=" + encodeURIComponent(user);
-
-    // Rediriger l'utilisateur vers la nouvelle page
-    window.location.href = nouvellePageURL;
-}
